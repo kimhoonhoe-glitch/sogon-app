22b61a9a8462814b94a714b6d3c925c8a16e10e8
kimhoonhoe-glitch <kimhoonhoe@gmail.com>
2025-10-22 12:50:35 +0000

    Vercel 최종 빌드 오류 수정 완료

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,12 +1,6 @@
-import Stripe from 'stripe'
-
-const stripeKey = process.env.STRIPE_SECRET_KEY
-
-export const stripe = stripeKey 
-  ? new Stripe(stripeKey, {
-      apiVersion: '2025-09-30.clover',
-    })
-  : null
-
+1 import Stripe from 'stripe'
+2 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+3   apiVersion: '2023-10-16', // 쉼표가 여기에 있어야 합니다!
+4 }); // 중괄호와 닫는 괄호로 마무리
 export const PREMIUM_PRICE = 5000
 export const FREE_DAILY_LIMIT = 999999 // 사실상 무제한
