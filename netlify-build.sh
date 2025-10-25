e2413bf8e70a9601078e320bb66b4381fe19063f
kimhoonhoe-glitch <kimhoonhoe@gmail.com>
2025-10-25 11:11:45 +0000

    Final fix: Added flutter clean command to resolve project root issue

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# ★★★ 중요: 폴더 이동(cd) 명령을 제거했습니다. 빌드는 루트에서 실행됩니다. ★★★
-
 # 1. Flutter SDK 설치
 git clone --depth 1 https://github.com/flutter/flutter.git -b stable "$HOME/flutter"
 
@@ -12,6 +10,9 @@ export PATH="$HOME/flutter/bin:$PATH"
 # 3. Flutter Web 도구 활성화 및 precache
 flutter config --enable-web
 flutter precache --web
+
+# ★★★ 추가된 명령어: 프로젝트 클린 (환경 인식에 도움을 줍니다) ★★★
+flutter clean
 
 # 4. 의존성 설치 및 웹 빌드 실행
 flutter pub get
