978de644cda259d4e78648c885ea6082e9b21ae2
kimhoonhoe-glitch <kimhoonhoe@gmail.com>
2025-10-26 00:38:08 +0000

    Final build script attempt: Remove --target to use default lib/main.dart

--- netlify-build.sh
+++ netlify-build.sh
@@ -16,5 +16,6 @@ flutter precache --web
 # 3. pubspec.yaml 파일의 의존성을 가져옵니다.
 flutter pub get
 
-# 4. 앱을 웹용으로 릴리즈 빌드합니다. (최종 경로 수정: lib/app.dart)
-flutter build web --release --target=lib/app.dart
+# 4. 앱을 웹용으로 릴리즈 빌드합니다. (최종 단순화: --target 옵션 제거)
+# Flutter가 lib/main.dart를 자동으로 찾게 합니다.
+flutter build web --release
